77fa4ecf53e4f6a82089b336c12950201565f31b
HAL13000 <haru.smile.13@gmail.com>
2023-11-07 19:02:23 +0100

    commit by Haru

--- src/api/authApi.js
+++ src/api/authApi.js
@@ -1,6 +1,8 @@
 import axiosClient from "./axiosClient";
 
 // params=(Registerに渡されるユーザーデータ) をserverの方にPost（渡す）
+// Postman のボディー要素に打ち込んでいたものがParamの中身
+
 const authApi = {
   register: (params) => axiosClient.post("auth/register", params),
 };
