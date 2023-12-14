823a1731929e1f5e570b706a45113f8c00c76ace
HAL13000 <haru.smile.13@gmail.com>
2023-12-14 17:24:55 +0100

    commit by Haru

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,28 @@
-import React from "react";
+import { LoadingButton } from "@mui/lab";
+import { Box } from "@mui/material";
+import React, { useState } from "react";
 
+const createMemo = () => {};
 export const Home = () => {
-  return <div>Home</div>;
+  const [loading, setLoading] = useState(false);
+  return (
+    <div>
+      <Box
+        sx={{
+          height: "100%",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <LoadingButton
+          variant="outlined"
+          onClick={() => createMemo}
+          loading="loading"
+        >
+          Create a Memo
+        </LoadingButton>
+      </Box>
+    </div>
+  );
 };
