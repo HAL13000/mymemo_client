cea22bbf39eeeef4fb9ad681465e44f8f6cc405a
HAL13000 <haru.smile.13@gmail.com>
2024-01-11 11:45:23 +0100

    commit by Haru

--- src/components/common/EmojiPicker.jsx
+++ src/components/common/EmojiPicker.jsx
@@ -1,17 +1,45 @@
 import { Box, Typography } from "@mui/material";
+import Picker from "@emoji-mart/react";
 import React, { useEffect, useState } from "react";
 
 const EmojiPicker = (props) => {
   const [selectedEmoji, setSelectedEmoji] = useState();
+  const [isShowPicker, setIsShowPicker] = useState(false);
+
   useEffect(() => {
     setSelectedEmoji(props.icon);
   }, [props.icon]);
+
+  const showPicker = () => setIsShowPicker(!isShowPicker);
+  const selectEmoji = (e) => {
+    // console.log(e);
+    const emojiCode = e.unified.split("-");
+    let codesArray = [];
+    emojiCode.forEach((el) => codesArray.push("0x" + el));
+    const emoji = String.fromCodePoint(...codesArray);
+    console.log(emoji);
+    setIsShowPicker(false);
+  };
   return (
     <div>
-      <Box>
-        <Typography variant="h3" fontWeight="700" sx={{ cursor: "pointer" }}>
+      <Box sx={{ position: "relative", width: "max-content" }}>
+        <Typography
+          variant="h3"
+          fontWeight="700"
+          sx={{ cursor: "pointer" }}
+          onClick={showPicker}
+        >
           {props.icon}
         </Typography>
+      </Box>
+      <Box
+        sx={{
+          display: isShowPicker ? "block" : "none",
+          position: "absolute",
+          zIndex: "100",
+        }}
+      >
+        <Picker onEmojiSelect={selectEmoji} />
       </Box>
     </div>
   );
